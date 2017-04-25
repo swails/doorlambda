ad834b913c0db06f2bf49157602959251f67c0ba
Jason Swails <jason.swails@gmail.com>
2017-04-25 15:28:42 -0400

    Fix setup.py to install in the correct place.

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
-setup(
-    name='GarageDoorLambda',
-    version='1.0.0',
-    packages=find_packages(),
-    py_modules=['lambda_handler'],
-)
+class CustomInstall(install):
+    """Ensures the module is placed at the root of the zip file."""
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.prefix = self.install_lib = ''
+
+if __name__ == '__main__':
+    import sys
+    if sys.version_info[:2] != (3, 6):
+        sys.exit('This package must be built with Python 3.6')
+    setup(name='GarageDoorLambda',
+          cmdclass=dict(install=CustomInstall),
+          version='1.0.0',
+          description='Garage door controlled access lambda',
+          author='Jason Swails',
+          author_email='jason.swails@gmail.com',
+          py_modules=['lambda_handler'],
+          packages=find_packages('.'),
+         )
